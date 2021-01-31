c8ee634eeba53fd295b224711987a5535b2200b0
Pantanetti <DPantanetti@traditum.com>
2021-01-30 21:06:43 -0300

    Primero Diesel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+declare var $: any;
+declare function init($);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'PrimeroDiesel';
+
+  @ViewChild('videoPlayer') videoplayer: any;
+
+  ngOnInit(): void {
+
+    // SLIDER
+    init($);
+
+    //this.videoplayer.nativeElement.play();
+  }
+
+  closeSideNav() {
+    if ($("body").hasClass("mini-sidebar")) {
+
+      $("body").removeClass("show-sidebar");
+      $(".nav-toggler i").toggleClass("ti-close");
+      $(".nav-toggler i").addClass("ti-menu");
+    }
+  }
+
+
+
+
+
 }
